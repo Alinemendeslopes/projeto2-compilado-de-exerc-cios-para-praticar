edc55a42c4720730b2edc90e07ddbb4551b3f9a4
Alinemendeslopes <aline2024mendes@gmail.com>
2024-08-22 00:01:51 -0300

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,23 @@
+// 1. Faça um Programa que mostre a mensagem "Olá mundo" na tela
+alert('Olá mundo')
+
+// 2. Faça um Programa que peça um número e então mostre a mensagem "O número informado foi [número]".
+let num = prompt('Digite um número')
+alert('O número informado foi ' + num)
+
+// 3. Faça um Programa que peça dois números e imprima a soma.
+let number1 = prompt('Digite um número')
+let number2 = prompt('Digite outro número')
+let soma1 = parseInt(number1) + parseInt(number2)
+alert('O resultado da soma é ' + soma1)
+
+//4. Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+let nota1 = prompt('Digite a nota do primeiro bimestre')
+let nota2 = prompt('Digite a nota do segundo bimestre')
+let nota3 = prompt('Digite a nota do terceiro bimestre')
+let nota4 = prompt('Digite a nota do quarto bimestre')
+let media = (parseInt(nota1)+parseInt(nota2)+parseInt(nota3)+parseInt(nota4)/4)
+alert('O Resultado da média é: ' + media)
+
+//5. Faça um Programa que converta metros para centímetros.
+let metros = prom
